3753afda5f56701da3bb3afff76169e6ec2526a8
IGYEONGMUN <doehdotn2@naver.com>
2025-01-17 09:28:24 +0900

    250117

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 let currentSection = 0;
-const sections = document.querySelectorAll('.section');
+const sections = document.querySelectorAll(".section");
 const totalSections = sections.length;
 
-window.addEventListener('wheel', (e) => {
+window.addEventListener("wheel", (e) => {
   e.preventDefault();
 
-  if (e.deltaY > 0 ) {
-    // 아래로 스크롤
+  if (e.deltaY > 0) {
     currentSection++;
-    
-  } else if (e.deltaY < 0 ) {
-    // 위로 스크롤
+  } else if (e.deltaY < 0) {
     currentSection--;
-    
   }
   console.log(currentSection);
-  
-  // 해당 섹션으로 스크롤
+
   window.scrollTo({
     top: currentSection * window.innerHeight,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 });
